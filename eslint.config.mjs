cf66364c27b39e34f081c842846ec3c2438368ff
sahil max <sahilrahaman585@gmail.com>
2025-04-13 01:24:50 +0530

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable react-hooks/rules-of-hooks rule globally
+      "react-hooks/rules-of-hooks": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
