7ff4a505913d3dddd844c74d4ce18b932dda52a3
sahil max <sahilrahaman585@gmail.com>
2025-04-13 01:39:07 +0530

    update

--- app/admin/adminlogin/page.tsx
+++ app/admin/adminlogin/page.tsx
@@ -1,10 +1,9 @@
-"use client";
+"use client"; // Make sure this is the very first line
+
 import React, { useState } from "react";
 import { supabase } from "@/app/lib/supabaseClient";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
-
-
 
 type LoginProps = {
   onSuccess: () => void;
@@ -15,34 +14,34 @@ const Login: React.FC<LoginProps> = () => {
   const [password, setPassword] = useState("");
   const router = useRouter();
 
- 
   const handleLogin = async () => {
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
     });
-  
+
     if (error) {
       toast.error("Login failed");
       return;
     }
-  
-    //Check if user is in admins table
+  //  check the table admin available or not //
     const { data: adminData } = await supabase
       .from("admins")
       .select("*")
       .eq("id", data.user.id)
       .single();
-  
+
     if (adminData) {
       toast.success("Admin login successful");
       router.push("/admin");
+      router.refresh();
     } else {
       toast.error("Access denied: Not an admin");
     }
   };
 
   
+
   return (
     <div className="fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-md border-[2px] border-[#E4E7E9] w-full max-w-sm pb-7">
@@ -70,13 +69,6 @@ const Login: React.FC<LoginProps> = () => {
         >
           Login
         </button>
-
-        {/* <p className="text-center my-3">
-          New admin?{" "}
-          <Link href="/admin/adsignup" className="text-blue-600 hover:underline">
-            Signup
-          </Link>
-        </p> */}
       </div>
     </div>
   );
